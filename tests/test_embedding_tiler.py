b752430db5620c0fac7b154474ffa0aa9646976b
Daven Quinn <code@davenquinn.com>
2024-07-21 12:46:52 -0500

    Test basic reading of tile data

--- tests/test_embedding_tiler.py
+++ tests/test_embedding_tiler.py
@@ -1,2 +1,17 @@
-def test_basic():
-    assert True
+from pytest import fixture
+from pathlib import Path
+from mapbox_vector_tile import decode, encode
+
+__here__ = Path(__file__).parent
+
+
+@fixture
+def tile_data():
+    file = __here__ / "fixtures" / "carto-z5-x6-y12.pbf"
+    with file.open("rb") as f:
+        return f.read()
+
+
+def test_read_tile(tile_data):
+    tile = decode(tile_data)
+    assert tile.keys() == {"units", "lines"}
